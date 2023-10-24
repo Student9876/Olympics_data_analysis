f03cc192b0e2d40c9fce648e508d4773e68e7b50
Student9876 <shouvik9876@gmail.com>
2023-10-24 23:37:12 +0530

    Created partitions in graph

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-# Target 
-''' 
-1.
-''' 
-
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 file_path1 = "dataset/athlete_events.csv"
 file_path2 = "dataset/noc_regions.csv"
 
-
 df1 = pd.read_csv(file_path1)
 df2 = pd.read_csv(file_path2)
-column_name1 = "Name"
-column_name2 = "Year"
-column_name3 = "Team"
-column_name_1 = "region"
-
 
 dictionary_1 = {
-
+}
+dictionary_2 ={
 }
 
 Country_list=[]
@@ -49,37 +36,25 @@ for index, row in df1.iterrows():
 
 dictionary_1=dict(sorted(dictionary_1.items()))
 
-
-# df_outputCSV = pd.DataFrame(dictionary_1)
-# df_outputCSV.to_csv('output.csv', index=False)
-
-# for keys, value in dictionary_1.items():
-#    print(keys,value)
-
-dictionary_2 ={
-
-}
-
 for keys, value in dictionary_1.items():
     for key, val in value.items():
         if(key==2010):
             dictionary_2[keys]=val
 
 
-
 items = list(dictionary_2.items())
-# Split the list of tuples in half
+# Split the list of dictionary in half
 first_ = items[:len(items)//2]
 second_ = items[len(items)//2:]
-# Create two separate dictionaries
 
-
+# Dividing further in 4 parts
 first_half  = first_[:len(first_)//2]
 second_half  = first_[len(first_)//2:]
 third_half  = items[:len(second_)//2]
 forth_half  = items[len(second_)//2:]
 
 
+# Create four separate dictionaries
 first_half_dict = dict(first_half)
 second_half_dict = dict(second_half)
 third_half_dict = dict(third_half)
