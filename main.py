18ca560e1c9dd0169a8774df6e9810fd93fa4020
Student9876 <shouvik9876@gmail.com>
2023-11-01 01:35:45 +0530

    Add multiple chart plot row wise

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ file_path2 = "dataset/noc_regions.csv"
 df1 = pd.read_csv(file_path1)
 df2 = pd.read_csv(file_path2)
 
+
+# Enter year here 
+year = 2016
+
+
+
 dictionary_1 = {
 }
 dictionary_2 ={
@@ -17,28 +23,28 @@ Country_list=[]
 
 
 for index,row in df2.iterrows():
-    Country_list.append(row['region'])
+    Country_list.append(row['NOC'])
 
 for index, row in df1.iterrows():
-    if row['Team'] in Country_list:
-        dictionary_1[row['Team']]={}
+    if row['NOC'] in Country_list:
+        dictionary_1[row['NOC']]={}
 
 
 for index, row in df1.iterrows():
-    if row['Team'] in Country_list:
-        dictionary_1[row['Team']][row['Year']]=0
+    if row['NOC'] in Country_list:
+        dictionary_1[row['NOC']][row['Year']]=0
 
 for index, row in df1.iterrows():
-    if row['Team'] in Country_list:
+    if row['NOC'] in Country_list:
         if row['Medal'] != 'NA':
-            dictionary_1[row['Team']][row['Year']]+=1
+            dictionary_1[row['NOC']][row['Year']]+=1
 
 
 dictionary_1=dict(sorted(dictionary_1.items()))
 
 for keys, value in dictionary_1.items():
     for key, val in value.items():
-        if(key==2010):
+        if(key==year):
             dictionary_2[keys]=val
 
 
@@ -75,23 +81,23 @@ values4 = list(forth_half_dict.values())
 
 
 # Initialise the subplot function using number of rows and columns 
-figure, axis = plt.subplots(2, 2)
+figure, axis = plt.subplots(4)
+figure.suptitle(f"Olympics medal winner of year {year}")
 
-
-axis[0, 0].bar(range(len(first_half)), values1, tick_label=names1)
-axis[0, 0].set_title("1st") 
+# axis[0].set_title("1st") 
+axis[0].bar(range(len(first_half)), values1, tick_label=names1)
   
 
-axis[0, 1].bar(range(len(second_half)), values2, tick_label=names2)
-axis[0, 1].set_title("2nd") 
+# axis[1].set_title("2nd") 
+axis[1].bar(range(len(second_half)), values2, tick_label=names2)
   
 
-axis[1, 0].bar(range(len(third_half)), values3, tick_label=names3)
-axis[1, 0].set_title("3rd") 
+# axis[2].set_title("3rd") 
+axis[2].bar(range(len(third_half)), values3, tick_label=names3)
   
  
-axis[1, 1].bar(range(len(forth_half)), values4, tick_label=names4)
-axis[1, 1].set_title("4th")
+# axis[3].set_title("4th")
+axis[3].bar(range(len(forth_half)), values4, tick_label=names4)
 
 
 
